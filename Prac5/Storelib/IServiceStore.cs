7da74c0c02f7c433c184d7d6568d20b8ea9d2dae
ruwan kellaert <ruwankellaert@hotmail.com>
2012-05-23 19:08:27 +0200

    added prac5

---
+++ Prac5/Storelib/IServiceStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Storelib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceStore" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceStore
+    {
+        [OperationContract]
+        void Login();
+        [OperationContract]
+        void signup();
+
+        [DataContract]
+        public class Product
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public double Price { get; set; }
+            [DataMember]
+            public Product(String name)
+            {
+                Name = name;
+            }
+        }
+
+        [DataContract]
+        public class Persoon
+        {
+            [DataMember]
+            public string Name { get; set; }
+            [DataMember]
+            public string Password { get; set; }
+            [DataMember]
+            public Persoon(String name)
+            {
+                Name = name;
+            }
+        }
+    }
+
+}
---
+++ Prac5/Storelib/ServiceStore.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Storelib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceStore" in both code and config file together.
+    public class ServiceStore : IServiceStore
+    {
+        public void Login()
+        {
+        }
+        public void Signup()
+        {
+        }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+    /*
+   public class Persoon
+   {
+       public string Name { get; set; }
+       public string Password { get; set; }
+   }
+
+   public class Store
+   {
+       public string product { get; set;
+   }*/
+    }
+}
